569a45c4fd1fce81723340f61167bac2239bc103
sains1 <26570164+sains1@users.noreply.github.com>
2024-02-17 12:20:46 +0000

    feat: adds get product REST endpoint - Controller layer - App handlers - Repo layer - Tests for each layer

--- src/Ordering/OrderingService/Program.cs
+++ src/Ordering/OrderingService/Program.cs
@@ -1,6 +1,13 @@
+using System.Reflection;
+
 using Microsoft.EntityFrameworkCore;
 
+using OrderingService.Application.Products;
+using OrderingService.Application.Products.GetProduct;
 using OrderingService.Infrastructure;
+using OrderingService.Infrastructure.Repositories;
+
+using SharedKernel.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +15,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 builder.Services.AddProblemDetails();
+builder.Services.AddOpenApiServices(Assembly.GetExecutingAssembly());
+builder.Services.AddControllers();
+
+// app handlers (this could use Assembly scanning, or a library like Mediatr)
+builder.Services.AddScoped<GetProductHandler>();
 
 // data layer
 builder.Services.AddDbContext<OrderingDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("OrderingDb")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("OrderingDb"), np => np.EnableRetryOnFailure()));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -24,7 +38,11 @@ if (builder.Environment.IsDevelopment())
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
-
 app.MapDefaultEndpoints();
+
+app.MapSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
 
 app.Run();
---
+++ src/Shared/SharedKernel/OpenApi/OpenApiHostingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SharedKernel.OpenApi;
+
+public static class OpenApiHostingExtensions
+{
+    public static IServiceCollection AddOpenApiServices(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(opts =>
+        {
+            // include xml docs
+            var xmlFilename = $"{assembly.GetName().Name}.xml";
+            opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
+        return services;
+    }
+}
