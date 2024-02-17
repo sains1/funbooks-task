661ac6631f1adf3c4e2c7c8df0aaac5b1d7ba620
sains1 <26570164+sains1@users.noreply.github.com>
2024-02-17 16:58:27 +0000

    tidy comments

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,5 +1,3 @@
-using System.Net.Sockets;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 var slnDirectory = GetSolutionDirectory(Directory.GetCurrentDirectory());
@@ -15,22 +13,20 @@ builder.AddProject<Projects.OrderingService>("orderingservice")
 
 builder.Build().Run();
 
+// recurse up the file tree until we find the solution file
 static string? GetSolutionDirectory(string directoryPath)
 {
-    // Check if the current directory contains a solution file
     string[] solutionFiles = Directory.GetFiles(directoryPath, "*.sln");
     if (solutionFiles.Length > 0)
     {
-        return solutionFiles[0]; // Return the first solution file found
+        return solutionFiles[0];
     }
 
-    // If not, move up to the parent directory
     string? parentDirectory = Directory.GetParent(directoryPath)?.FullName;
     if (parentDirectory != null)
     {
-        return GetSolutionDirectory(parentDirectory); // Recurse up the tree
+        return GetSolutionDirectory(parentDirectory);
     }
 
-    // If no solution file is found in any parent directory, return null
     return null;
 }
