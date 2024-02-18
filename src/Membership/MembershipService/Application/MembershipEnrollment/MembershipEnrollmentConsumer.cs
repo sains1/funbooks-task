adf3b5917451d5b8ee1de8d39e399e8a5fe3fe2e
sains1 <26570164+sains1@users.noreply.github.com>
2024-02-18 19:35:14 +0000

    feat: adds membership service (#8)
    - limited implementation due to time

---
+++ src/Membership/MembershipService/Application/MembershipEnrollment/MembershipEnrollmentConsumer.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+
+using OrderingService.Contracts.Events;
+
+namespace MembershipService.Application.MembershipEnrollment;
+
+public class MembershipEnrollmentConsumer(IMembershipRepository repository)
+    : IConsumer<OrderSubmitted>
+{
+    public async Task Consume(ConsumeContext<OrderSubmitted> context)
+    {
+        var membershipProducts = context.Message.LineItems.Where(x => x.ProductType == ProductType.Membership).ToList();
+        if (!membershipProducts.Any())
+        {
+            return; // nothing to do
+        }
+
+        foreach (var membershipProduct in membershipProducts)
+        {
+            await repository.EnrollUserInMembership(context.Message.CustomerId, membershipProduct.ProductId);
+        }
+    }
+}
---
+++ src/Membership/MembershipService/Program.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+
+using MassTransit;
+
+using MembershipService.Application.MembershipEnrollment;
+using MembershipService.Infrastructure.Repositories;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.Configure<MassTransitHostOptions>(options =>
+{
+    options.WaitUntilStarted = true;
+});
+
+builder.Services.AddMassTransit(options =>
+{
+    options.SetKebabCaseEndpointNameFormatter();
+
+    var entryAssembly = Assembly.GetEntryAssembly();
+
+    options.AddConsumers(entryAssembly);
+    options.AddActivities(entryAssembly);
+
+    options.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], h =>
+        {
+            h.Password(builder.Configuration["RabbitMq:Password"]);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddScoped<IMembershipRepository, DebugMembershipRepository>();
+
+var host = builder.Build();
+host.Run();
